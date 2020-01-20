d874c71f0c5e30d55533a4e110456bea1bd5d4a6
Ivan Pleskonjic <ivan.pleskonjic@logate.com>
2020-01-20 20:44:02 +0100

    Updates

---
+++ precache-manifest.f4c00a8214cdc123c217b5cac1b15c6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7d804c430889eef4b47c7d04d6723ef",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "3cdfdc78d0e2a6d7e320",
+    "url": "/react-todo-app/static/css/main.3689dbae.chunk.css"
+  },
+  {
+    "revision": "28890d23c2e738f9450a",
+    "url": "/react-todo-app/static/js/2.d49fc373.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-todo-app/static/js/2.d49fc373.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3cdfdc78d0e2a6d7e320",
+    "url": "/react-todo-app/static/js/main.d93d72c3.chunk.js"
+  },
+  {
+    "revision": "ccb165e84fd24aa3397e",
+    "url": "/react-todo-app/static/js/runtime-main.c16cf18c.js"
+  }
+]);
